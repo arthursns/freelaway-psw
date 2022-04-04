405ae6a31312638bc097f0ffc227b6fc7c170b86
Arthur Silva <arthursns@gmail.com>
2022-04-04 16:54:22 -0300

    :wrench: fix: Configuração da Secret Key do Django

--- freelaway/settings.py
+++ freelaway/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from decouple import config
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p1nj$#c#&aoz19#w^bc91a7@#$+y#je+13xq2izr=6wu$nw8@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
